d32b63c5e79921531a7a2a9f0c4d4299e17ed2cb
Fabian Bur <fabian.bur@students.fhnw.ch>
2018-01-03 23:36:54 +0100

    shrink navbar when scrolled

--- js/navbar.js
+++ js/navbar.js
@@ -5,3 +5,18 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+function resizeHeaderOnScroll() {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+    shrinkOn = 200,
+    headerEl = document.getElementById('sideNav');
+    
+    if (distanceY > shrinkOn) {
+      headerEl.classList.add("smaller");
+    } else {
+      headerEl.classList.remove("smaller");
+    }
+  }
+  
+  window.addEventListener('scroll', resizeHeaderOnScroll);
+  
